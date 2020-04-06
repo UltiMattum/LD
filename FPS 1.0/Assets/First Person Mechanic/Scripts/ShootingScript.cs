096207edffa27fc7e1dd9c57f489afcbadc84e5a
IndieDevAdam <60450823+IndieDevAdam@users.noreply.github.com>
2020-04-06 20:29:50 +0100

    Redoing The GunScript

---
+++ FPS 1.0/Assets/First Person Mechanic/Scripts/ShootingScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingScript : MonoBehaviour
+{
+    private float Damage = 12f;
+    private float Range = 100;
+
+    public Camera fpsCAM;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+     void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCAM.transform.position, fpsCAM.transform.forward, out hit, Range))
+        {
+            Debug.Log(hit.transform.name);
+
+
+
+        }
+
+    }
+}
