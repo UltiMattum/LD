bcadc146c9ead9af2f82bc2e7a9011bb17a61a12
Matthew Borres <matt_931998@yahoo.com.ph>
2020-04-07 13:37:57 +0800

    Some changes

---
+++ FPS 1.0/Assets/1.0.0/Main/Scripts/ShootingScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingScript : MonoBehaviour
+{
+    private float Damage = 12f;
+    private float Range = 100;
+    public Camera fpsCAM;
+
+    void Update()
+    {
+        if(Input.GetButton("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+     void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCAM.transform.position, fpsCAM.transform.forward, out hit, Range))
+        {
+            Debug.Log(hit.transform.name);
+        }
+        
+    }
+}
