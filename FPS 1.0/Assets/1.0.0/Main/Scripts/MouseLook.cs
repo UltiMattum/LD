c175dac868943c10654e67032a1bc9e6352f2d6b
Matthew Borres <matt_931998@yahoo.com.ph>
2020-04-14 13:08:23 +0800

    Just did some test.

--- FPS 1.0/Assets/1.0.0/Main/Scripts/MouseLook.cs
+++ FPS 1.0/Assets/1.0.0/Main/Scripts/MouseLook.cs
@@ -26,8 +26,9 @@ public class MouseLook : MonoBehaviour {
             currentX = Mathf.SmoothDamp(currentX, xRot, ref xRotationV, lookSmoothDamp);
             currentY = Mathf.SmoothDamp(currentY, yRot, ref yRotationV, lookSmoothDamp);
 
-            // This line restricts the yRotation value to be less than 80 and more than -80
+            // This line restricts the xRotation value to be less than 80 and more than -80
             xRot = Mathf.Clamp (xRot, -80, 80);
+            // yRot = Mathf.Clamp (yRot, -80, 80);
             // Setting the rotation of the camera according to mouse input
             transform.rotation = Quaternion.Euler (-currentX, currentY, 0);
         }
